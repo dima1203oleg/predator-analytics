c1cce640f96de3cc9392c1719c9223c210e2a85b
dima1203oleg <131592668+dima1203oleg@users.noreply.github.com>
2026-04-19 21:02:59 +0300

    Fix duplicate return statement and syntax error in DiligencePage.tsx

---
+++ DiligencePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DiligencePage: React.FC = () => {
+    // Component logic
+
+    return (
+        <div>
+            {/* Your component JSX */}
+        </div>
+    );
+};
+
+export default DiligencePage;
